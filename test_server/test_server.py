14025638fc88df2bc2c338b4feb33c2057483ab7
GFGH <12trikitik21@gmail.com>
2024-02-10 17:31:24 +0000

    Update of a test_server

--- test_server/test_server.py
+++ test_server/test_server.py
@@ -1,45 +1,45 @@
 import socket
 
-serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #семья и тип сервера в скобках
-IP = '127.0.0.1' #socket.gethostbyname(socket.gethostname())
-PORT = 1233
-serv_socket.bind((IP, PORT)) 
-#serv_socket = socket.create_server((IP, PORT)) то же, что и выше - создание самого сервера
-serv_socket.listen(4) #кол-во принимаемых запросов для режима ожидания
 
-print('Start working...')
-print(IP) 
+def start_the_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #семья и тип сервера в скобках
+        IP = '127.0.0.1' #socket.gethostbyname(socket.gethostname())
+        PORT = 1233
+        server.bind((IP, PORT)) 
+        #server = socket.create_server((IP, PORT)) то же, что и выше - создание самого сервера
+        server.listen(4) #кол-во принимаемых запросов для режима ожидания
 
-client, address = serv_socket.accept() #принятие запроса с разделением на клиента и его адрес, ждем сигнала.
+        print('Start working...')
+        
+        while True:
+            client, address = server.accept() #принятие запроса с разделением на клиента и его адрес, ждем сигнала.
+            data = client.recv(1024).decode('utf8')
 
-data = client.recv(1024).decode('utf8')
+            print(data, "\n\n\n\n")
+            
+            content = load_page_of_request(data)
+            client.send(content)
+            client.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print('\nShutdown...')
 
-print(data)
-content = ''
-content += "▒▒▒▒▒█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"
-content += '\n'
-content += "▒▒▒▒▒█░▒▒▒▒▒▒▒▓▒▒▓▒▒▒▒▒▒▒░█"
-content += '\n'
-content += "▒▒▒▒▒█░▒▒▓▒▒▒▒▒▒▒▒▒▄▄▒▓▒▒░█░▄▄"
-content += '\n'
-content += "▄▀▀▄▄█░▒▒▒▒▒▒▓▒▒▒▒█░░▀▄▄▄▄▄▀░░█"
-content += '\n'
-content += "█░░░░█░▒▒▒▒▒▒▒▒▒▒▒█░░░░░░░░░░░█"
-content += '\n'
-content += "▒▀▀▄▄█░▒▒▒▒▓▒▒▒▓▒█░░░█▒░░░░█▒░░█"
-content += '\n'
-content += "▒▒▒▒▒█░▒▓▒▒▒▒▓▒▒▒█░░░░░░░▀░░░░░█"
-content += '\n'
-content += "▒▒▒▄▄█░▒▒▒▓▒▒▒▒▒▒▒█░░█▄▄█▄▄█░░█"
-content += '\n'
-content += "▒▒▒█░░░█▄▄▄▄▄▄▄▄▄▄█░█▄▄▄▄▄▄▄▄▄█"
-content += '\n'
-content += "▒▒▒█▄▄█░░█▄▄█░░░░░░█▄▄█░░█▄▄█"
-content += '\n'
-content += "I've done it, boss <3"
-content = content.encode("UTF8")
-HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain;charset=UTF8\r\nConnection: close\r\n'
-HDRS += 'Content-Length: ' + str(len(content)) + '\r\n\r\n'
-HDRS = HDRS.encode('UTF8')
-client.send(HDRS + content)
-print('Shutdown...')
+
+def load_page_of_request(request_data):
+    path = request_data.split(' ')[1]
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=UTF8\r\nConnection: close\r\n'
+    content = ''
+    try:
+        with open('views' + path, 'rb') as file:
+            content = file.read()
+        HDRS += 'Content-Length: ' + str(len(content)) + '\r\n\r\n'
+        HDRS = HDRS.encode('UTF8')
+        return HDRS + content
+    except FileNotFoundError:
+        HDRS_404 = 'HTTP/1.0 404 Not Found\r\n\r\n'
+        return (HDRS_404 + 'Sorry, we have not page like that yet...').encode('UTF8')
+    
+
+
+start_the_server()

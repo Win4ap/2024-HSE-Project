df81be31ee1ab439b94c3f21bccbc1372cb6db47
Igor Berdov <berdov.igor@gmail.com>
2024-02-15 15:47:20 +0300

    Setup BoxLayout

--- src/main.py
+++ src/main.py
@@ -2,11 +2,15 @@ import kivy
 kivy.require('2.3.0')
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
 Window.size = (360, 640)
 Window.clearcolor = (64/255, 64/255, 64/255, 1)
 Window.title = "Delivery: Fast&Smart"
+
+class Container(BoxLayout):
+    pass
 
 class DFSApp(App):
     def build(self):

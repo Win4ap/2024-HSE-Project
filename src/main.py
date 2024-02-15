3fd2a7dee7fdd134e7e40d12222a7c40008e48cb
Win4ap <mrwinchap@gmail.com>
2024-02-15 15:02:19 +0300

    Made 340x640 as main resolution

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ kivy.require('2.3.0')
 from kivy.app import App
 from kivy.core.window import Window
 
-Window.size = (300, 500)
+Window.size = (360, 640)
 Window.clearcolor = (64/255, 64/255, 64/255, 1)
 Window.title = "Delivery: Fast&Smart"
 

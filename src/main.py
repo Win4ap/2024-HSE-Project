7d260f1fc9c49bc1b653508251289f8eaa3bbc4e
Win4ap <mrwinchap@gmail.com>
2024-02-15 22:09:36 +0300

    Full GUI without server requests

--- src/main.py
+++ src/main.py
@@ -3,28 +3,56 @@ kivy.require('2.3.0')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 
 Window.size = (360, 640)
-Window.clearcolor = (64/255, 64/255, 64/255, 1)
+Window.clearcolor = (60/255, 60/255, 60/255, 1)
 Window.title = "Delivery: Fast&Smart"
 
-class Container(BoxLayout):
-    client_state = ObjectProperty()
-    delivery_state = ObjectProperty()
+class RoundedButton_auth(Button):
+    pass
+
+class LeftRoundedToggleButton_auth(ToggleButton):
+    pass
+
+class RightRoundedToggleButton_auth(ToggleButton):
+    pass
+
+class AuthWindow(BoxLayout):
+    client_switch = ObjectProperty()
+    delivery_switch = ObjectProperty()
+    login_input = ObjectProperty()
+    password_input = ObjectProperty()
+    password_hide_button = ObjectProperty()
+
 
     def change_delivery(self):
-        self.client_state.state = 'down'
-        self.delivery_state.state = 'normal'
+        self.client_switch.state, self.delivery_switch.state = 'down', 'normal'
     
     def change_client(self):
-        self.client_state.state = 'normal'
-        self.delivery_state.state = 'down'
+        self.client_switch.state, self.delivery_switch.state = 'normal', 'down'
+
+    def show_password(self):
+        self.password_input.password = False if self.password_hide_button.state == 'down' else True
+
+
+    # placeholder yet, just say thx to god 'cause it is working
+    def send_login_request(self):
+        if (self.login_input.text == '' or self.password_input.text == ''):
+            print('Логин / пароль не может быть пустым!')
+        else:
+            status = 'курьер'
+            if self.client_switch.state == 'down':
+                status = 'клиент'
+            print(f'Попытка входа: {status}, логин: {self.login_input.text}, пароль: {self.password_input.text}')
+
 
 class DFSApp(App):
     def build(self):
-        return Container()
+        return AuthWindow()
 
 
 if __name__ == '__main__':

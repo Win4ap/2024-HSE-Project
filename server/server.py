db9fc91bf05bae9a4fe3d686782a5b9622293407
GFGH <142992199+1gfgh@users.noreply.github.com>
2024-03-03 20:29:30 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ def start_the_server():
         IP = '127.0.0.1' 
         PORT = 1233
         server.bind((IP, PORT)) 
-        server.listen(4) 
+        server.listen(10)
 
         print('Start working...')
         
@@ -27,6 +27,7 @@ def start_the_server():
                 server.close()
                 return
             client.send(content.encode('utf8'))
+            client.shutdown(socket.SHUT_WR)
     except KeyboardInterrupt:
         server.close()
         print('\nShutdown...')
@@ -34,45 +35,52 @@ def start_the_server():
 
 def process_the_request(request_data):
     request = request_data.split(' ')
-    match request[0]:
+    state = request[0]
+    match state:
         case 'shutdown':
-                return 'SHUTDOWN'
-        case 'register':
-            state = request[1]
-            login_input = request[2]
-            password_input = request[3]
-            if state == 'client':
-                if client_logins_passwords.get(login_input) != None:
-                    return 'login_exists'
-                client_logins_passwords[login_input] = password_input
-                return 'done_successfully'
-            if state == 'delivery':
-                if delivery_logins_passwords.get(login_input) != None:
-                    return 'login_exists'
-                delivery_logins_passwords[login_input] = password_input
-                return 'done_successfully'
-            return 'Error'
-        case 'login':
-            state = request[1]
-            login_input = request[2]
-            password_input = request[3]
-            if state == 'client':
-                if client_logins_passwords.get(login_input) == None:
-                    return 'login_doesnt_exists'
-                if client_logins_passwords[login_input] == password_input:
-                    return 'correct'
-                else:
-                    return 'incorrect'
-            if state == 'delivery':
-                if delivery_logins_passwords.get(login_input) == None:
-                    return 'login_doesnt_exists'
-                if delivery_logins_passwords[login_input] == password_input:
-                    return 'correct'
-                else:
-                    return 'incorrect'
-            return 'Error'
+            return 'SHUTDOWN'
+        case 'client':
+            match request[1]:
+                case 'register':
+                    login_input = request[2]
+                    password_input = request[3]
+                    if client_logins_passwords.get(login_input) != None:
+                        return 'login_exists'
+                    client_logins_passwords[login_input] = password_input
+                    return 'done_successfully'
+                case 'login':
+                    login_input = request[2]
+                    password_input = request[3]
+                    if client_logins_passwords.get(login_input) == None:
+                        return 'login_doesnt_exists'
+                    if client_logins_passwords[login_input] == password_input:
+                        return 'correct ' + state + ' ' + login
+                    else:
+                        return 'incorrect'
+                case _:
+                    return 'error_of_request'
+        case 'delivery':
+            match request[1]:
+                case 'register':
+                    login_input = request[2]
+                    password_input = request[3]
+                    if delivery_logins_passwords.get(login_input) != None:
+                        return 'login_exists'
+                    delivery_logins_passwords[login_input] = password_input
+                    return 'done_successfully'
+                case 'login':
+                    login_input = request[2]
+                    password_input = request[3]
+                    if delivery_logins_passwords.get(login_input) == None:
+                        return 'login_doesnt_exists'
+                    if delivery_logins_passwords[login_input] == password_input:
+                        return 'correct ' + state + ' ' + login
+                    else:
+                        return 'incorrect'
+                case _:
+                    return 'error_of_request'
         case _:
-            return 'Error'
+            return 'error_of_state'
 
 
 start_the_server()

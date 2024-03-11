1944b1bbb3be7c68077f199b6f70c850ef603a0b
Win4ap <mrwinchap@gmail.com>
2024-03-12 00:42:59 +0300

    constants -> constants.py

--- src/windows/server_logic/server_interaction.py
+++ src/windows/server_logic/server_interaction.py
@@ -1,24 +1,15 @@
 import socket
 import os
 import logging
+from windows.server_logic.constants import IP, PORT
 
 class ServerLogic():
-    constants = []
-    try:
-        path_to_constants = os.path.join(os.getcwd(), 'src', 'windows', 'server_logic', 'constants')
-        with open(path_to_constants, 'r') as f:
-            constants = (f.read()).split(' ')
-    except FileNotFoundError:
-        logging.error('Cannot found file with constants!')
-    IP = constants[0]
-    PORT = int(constants[1])
-
     def auth_reg_request(self, state, command, login, password) -> str:
         request = f'{state} {command} {login} {password}'
         logging.info(f'{command}: {state} {login}')
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             try:
-                client.connect((self.IP, self.PORT))
+                client.connect((IP, PORT))
                 client.send(request.encode('utf8'))
                 answer = client.recv(1024).decode('utf8')
                 logging.info(f'Server answer: {answer}')
@@ -37,7 +28,7 @@ class ServerLogic():
         logging.info(f'get_user_{info}: {state} {login}')
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             try:
-                client.connect((self.IP, self.PORT))
+                client.connect((IP, PORT))
                 client.send(request.encode('utf8'))
                 answer = client.recv(1024).decode('utf8')
                 logging.info(f'Server answer: {answer}')

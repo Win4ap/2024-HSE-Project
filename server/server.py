672cf909ab6326038c3a87780f1c5356d3ac2fd0
GFGH <12trikitik21@gmail.com>
2024-03-14 18:12:46 +0000

    Add edit_profile case

--- server/server.py
+++ server/server.py
@@ -9,13 +9,13 @@ path_to_database = os.path.join(
 
 
 def start_the_server():
-    logging.basicConfig(level=logging.DEBUG, filename="loggings.log",
+    logging.basicConfig(level=logging.INFO, filename="loggings.log",
                         filemode="w", format="%(asctime)s %(levelname)s %(message)s")
     with sqlite3.connect(path_to_database) as database:
         cursor = database.cursor()
-        query = """ CREATE TABLE IF NOT EXISTS client_logins_passwords ( login TEXT, password TEXT, fullness INTEGER ) """
+        query = """ CREATE TABLE IF NOT EXISTS client_data ( login TEXT, password TEXT, name TEXT, surname TEXT, phone_number TEXT, fullness INTEGER ) """
         cursor.execute(query)
-        query = """ CREATE TABLE IF NOT EXISTS delivery_logins_passwords ( login TEXT, password TEXT, fullness INTEGER ) """
+        query = """ CREATE TABLE IF NOT EXISTS delivery_data ( login TEXT, password TEXT, name TEXT, surname TEXT, phone_number TEXT, fullness INTEGER ) """
         cursor.execute(query)
         query = """ CREATE TABLE IF NOT EXISTS orders_list ( id INTEGER, login TEXT, name TEXT, cost INTEGER, description TEXT ) """
         cursor.execute(query)
@@ -23,14 +23,16 @@ def start_the_server():
         cursor.execute(query)
         database.commit()
     try:
+        global server
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         IP = constants.IP
         PORT = constants.PORT
         server.bind((IP, PORT))
-        server.listen(10)
-        logging.debug('Server has started!')
+        server.listen()
+        logging.info('Server has started!')
         while True:
             # принятие запроса с разделением на клиента и его адрес, ждем сигнала.
+            global client
             client, address = server.accept()
             data = client.recv(1024).decode('utf8')
             logging.info('Some data have received:\n\n' + data + '\n')
@@ -88,6 +90,12 @@ def process_the_request(request_data):
                     logging.debug('client get_profile_fullness case')
                     login = request[2]
                     return get_profile_fullness(login, state)
+                case 'edit_profile':
+                    login = request[2]
+                    name = request[3]
+                    surname = request[4]
+                    phone = request[5]
+                    return edit_profile(state, login, name, surname, phone)
                 case _:
                     return 'error error_of_request'
         case 'delivery':
@@ -102,6 +110,10 @@ def process_the_request(request_data):
                     login = request[2]
                     password_input = request[3]
                     return try_to_login(login, password_input, state)
+                case 'get_profile_fullness':
+                    logging.debug('client get_profile_fullness case')
+                    login = request[2]
+                    return get_profile_fullness(login, state)
                 case _:
                     return 'error error_of_request'
         case _:
@@ -109,15 +121,15 @@ def process_the_request(request_data):
 
 
 def try_to_register(login, password, state) -> str:
-    logging.debug('try to register')
+    logging.info('try to register')
     with sqlite3.connect(path_to_database) as database:
         logging.debug('connected to database')
         cursor = database.cursor()
-        query = f""" SELECT login FROM {state}_logins_passwords WHERE login = ? """
+        query = f""" SELECT login FROM {state}_data WHERE login = ? """
         cursor.execute(query, (login,))
         user = cursor.fetchone()
         if user == None:
-            query = f""" INSERT INTO {state}_logins_passwords (login, password, fullness) VALUES (?, ?, ?) """
+            query = f""" INSERT INTO {state}_data (login, password, fullness) VALUES (?, ?, ?) """
             cursor.execute(query, (login, password, 0))
             database.commit()
             return 'done'
@@ -126,11 +138,11 @@ def try_to_register(login, password, state) -> str:
 
 
 def try_to_login(login, password, state) -> str:
-    logging.debug('try to login')
+    logging.info('try to login')
     with sqlite3.connect(path_to_database) as database:
         logging.debug('connected to database')
         cursor = database.cursor()
-        query = f""" SELECT * FROM {state}_logins_passwords WHERE login = ? """
+        query = f""" SELECT * FROM {state}_data WHERE login = ? """
         cursor.execute(query, (login,))
         user = cursor.fetchone()
         if user == None:
@@ -143,12 +155,12 @@ def try_to_login(login, password, state) -> str:
 
 
 def make_new_order(login, name, cost, description) -> str:
-    logging.debug('make new order')
+    logging.info('make new order')
     cur_id = -1
     with sqlite3.connect(path_to_database) as database:
         logging.debug('connected to database')
         cursor = database.cursor()
-        query = """ SELECT login FROM client_logins_passwords WHERE login = ? """
+        query = """ SELECT login FROM client_data WHERE login = ? """
         cursor.execute(query, (login,))
         if cursor.fetchone() == None:
             return 'error login_doesnt_exists'
@@ -174,11 +186,11 @@ def make_new_order(login, name, cost, description) -> str:
 
 
 def make_new_template(login, name, cost, description) -> str:
-    logging.debug('make new template')
+    logging.info('make new template')
     with sqlite3.connect(path_to_database) as database:
         logging.debug('connected to database')
         cursor = database.cursor()
-        query = """ SELECT login FROM client_logins_passwords WHERE login = ? """
+        query = """ SELECT login FROM client_data WHERE login = ? """
         cursor.execute(query, (login,))
         if cursor.fetchone() == None:
             return 'error login_doesnt_exists'
@@ -189,12 +201,12 @@ def make_new_template(login, name, cost, description) -> str:
 
 
 def get_user_data(login, data) -> str:
-    logging.debug(f"get user's {data}")
+    logging.info(f"get user's {data}")
     result = 'done '
     with sqlite3.connect(path_to_database) as database:
         logging.debug('connected to database')
         cursor = database.cursor()
-        query = """ SELECT login FROM client_logins_passwords WHERE login = ? """
+        query = """ SELECT login FROM client_data WHERE login = ? """
         cursor.execute(query, (login,))
         if cursor.fetchone() == None:
             return 'error login_doesnt_exists'
@@ -211,21 +223,62 @@ def get_user_data(login, data) -> str:
 
 
 def get_profile_fullness(login, state) -> str:
+    logging.info(f'getting profile fullness: {state} {login}')
     result = 'done '
     with sqlite3.connect(path_to_database) as database:
         logging.debug('connected to database')
         cursor = database.cursor()
-        query = """ SELECT login FROM client_logins_passwords WHERE login = ? """
+        query = f""" SELECT login FROM {state}_data WHERE login = ? """
         cursor.execute(query, (login,))
         if cursor.fetchone() == None:
             return 'error login_doesnt_exists'
-        query = f""" SELECT fullness FROM {state}_logins_passwords WHERE login = ? """
+        query = f""" SELECT fullness FROM {state}_data WHERE login = ? """
         cursor.execute(query, (login,))
         fullness = cursor.fetchone()[0]
         if fullness:
             result += 'True'
         else:
             result += 'False'
+    return result
+
+
+def edit_profile(state, login, name, surname, phone) -> str:
+    logging.info(
+        f'{state} {login} editing profile with a data: ' + 
+        f'name = {name}, surname = {surname}, phone = {phone}')
+    result = 'done '
+    path_to_profile_picture = os.path.join(
+        os.getcwd(), 'images', f'{state}_{login}_profile_picture.jpg')
+    size = int(client.recv(1024))
+    logging.debug(f'Got size of picture: {size}')
+    processed_size = 0
+    with open(path_to_profile_picture, mode = 'wb') as profile_picture:
+        while processed_size < size:
+            data = client.recv(2048)
+            profile_picture.write(data)
+            processed_size += len(data)
+            logging.debug(f'processed size if {processed_size} of {size}')
+    logging.debug('Done with profile picture')
+    client.send(('done').encode('utf8'))
+    path_to_passport = os.path.join(
+        os.getcwd(), 'images', f'{state}_{login}_passport.jpg')
+    logging.debug('Try to get size of passport image')
+    size = int(client.recv(1024))
+    logging.debug(f'Got size of passport: {size}')
+    processed_size = 0
+    with open(path_to_passport, mode = 'wb') as passport:
+        while processed_size < size:
+            data = client.recv(2048)
+            passport.write(data)
+            processed_size += len(data)
+            logging.debug(f'processed size if {processed_size} of {size}')
+    logging.debug('Done with passport picture')
+    with sqlite3.connect(path_to_database) as database:
+        logging.debug('connected to database')
+        cursor = database.cursor()
+        query = f""" UPDATE {state}_data SET name = ?, surname = ?, phone_number = ?, fullness = ? WHERE login = ?"""
+        cursor.execute(query, (name, surname, phone, 1, login))
+        database.commit()
     return result
 
 

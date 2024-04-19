2d94674d846512d361acea4c8ca771861219ad8d
Win4ap <mrwinchap@gmail.com>
2024-04-19 17:17:14 +0300

    Base switching between screens in delivery sm

--- src/windows/delivery.py
+++ src/windows/delivery.py
@@ -4,4 +4,19 @@ from windows.baseclass import ColorAnimBase
 from windows.server_logic.server_interaction import ServerLogic
 
 class DeliverySide(Screen, ColorAnimBase, ServerLogic):
-    pass
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.delivery_main_frame.current = 'delivery_orders'
+
+    def switch_main_to(self, screen):
+        if self.delivery_main_frame.current != screen:
+            self.delivery_main_frame.current = screen
+        if screen == 'delivery_chat':
+            self.icon_chat.source, self.icon_list.source, self.icon_user.source = 'img/bold_chat.png', 'img/list.png', 'img/user.png'
+        elif screen == 'delivery_orders':
+            self.icon_chat.source, self.icon_list.source, self.icon_user.source = 'img/chat.png', 'img/bold_list.png', 'img/user.png'
+        elif screen == 'delivery_profile':
+            self.icon_chat.source, self.icon_list.source, self.icon_user.source = 'img/chat.png', 'img/list.png', 'img/bold_user.png'
+
+    def show_profile(self):
+        pass

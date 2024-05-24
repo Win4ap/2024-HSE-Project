ea7d6fe25d49cc258c674a6e205dd09f9a2e930c
Win4ap <mrwinchap@gmail.com>
2024-05-24 22:12:55 +0300

    Fix fatal error after request than server is off

--- src/windows/server_logic/server_interaction.py
+++ src/windows/server_logic/server_interaction.py
@@ -20,9 +20,6 @@ class ServerLogic():
         elif answer.status_code == 404 or answer.status_code == 403 or answer.status_code == 423:
             answer = answer.json()
             return answer['detail']
-        else:
-            logging.info('Server is down')
-            return 'server_error'
 
     def get_login(self) -> list:
         path_to_login = os.path.join(os.getcwd(), 'src', 'windows', 'server_logic', 'state_login')
@@ -34,12 +31,16 @@ class ServerLogic():
         password = password.encode('utf-8')
         password = encrypt(password, pubkey)
         logging.info(f'{command}: {state} {login}')
-        if command == 'login':
-            answer = requests.get(f'{URL}/{command}', data={'state': f'{state}', 'login': f'{login}'}, files={'password': password})
-        elif command == 'register':
-            answer = requests.post(f'{URL}/{command}', data={'state': f'{state}', 'login': f'{login}'}, files={'password': password})
-        else:
-            return 'FATAL'
+        try:
+            if command == 'login':
+                answer = requests.get(f'{URL}/{command}', data={'state': f'{state}', 'login': f'{login}'}, files={'password': password})
+            elif command == 'register':
+                answer = requests.post(f'{URL}/{command}', data={'state': f'{state}', 'login': f'{login}'}, files={'password': password})
+            else:
+                return 'FATAL'
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_client_data(self, info):
@@ -47,7 +48,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_user_{info}: {state} {login}')
-        answer = requests.get(f'{URL}/get_user_{info}', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_user_{info}', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_free_orders(self):
@@ -55,7 +60,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_free_orders: {state} {login}')
-        answer = requests.get(f'{URL}/get_free_orders', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_free_orders', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_delivery_orders(self):
@@ -63,7 +72,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_delivery_active_orders: {state} {login}')
-        answer = requests.get(f'{URL}/get_active_orders', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_active_orders', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_in_process_orders(self):
@@ -71,7 +84,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_in_process_orders: {state} {login}')
-        answer = requests.get(f'{URL}/get_in_process_orders', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_in_process_orders', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_auction_orders(self):
@@ -79,7 +96,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_auction_orders: {state} {login}')
-        answer = requests.get(f'{URL}/get_auction_orders', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_auction_orders', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_profile_fullness(self):
@@ -87,7 +108,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_profile_fullness: {state} {login}')
-        answer = requests.get(f'{URL}/get_profile_fullness', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_profile_fullness', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def edit_profile(self, firstname, lastname, phone, path_to_avatar, path_to_passport):
@@ -95,7 +120,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'edit_profile: {state} {login} {firstname} {lastname} {phone} {path_to_avatar} {path_to_passport}')
-        answer = requests.post(f'{URL}/upload_user_info', data={'state': f'{state}', 'login': f'{login}', 'name': f'{firstname}', 'surname': f'{lastname}', 'phone': f'{phone}'}, files={'profile_picture': open(path_to_avatar, mode = 'rb'), 'passport': open(path_to_passport, mode = 'rb')})
+        try:
+            answer = requests.post(f'{URL}/upload_user_info', data={'state': f'{state}', 'login': f'{login}', 'name': f'{firstname}', 'surname': f'{lastname}', 'phone': f'{phone}'}, files={'profile_picture': open(path_to_avatar, mode = 'rb'), 'passport': open(path_to_passport, mode = 'rb')})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_profile_data(self):
@@ -103,26 +132,34 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_profile: {state} {login}')
-        if self.get_profile_fullness() == 'false':
-            return 'Not Found'
-        path = os.path.join(os.getcwd(), 'src', 'windows', 'profile', 'avatar.jpg')
-        answer = requests.get(f'{URL}/get_user_picture/profile_picture',  json={'state': f'{state}', 'login': f'{login}'})
-        if answer.status_code == 200:
-            with open(path, mode='wb') as file:
-                file.write(answer.content)
-            answer = requests.get(f'{URL}/get_user_info', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            if self.get_profile_fullness() == 'false':
+                return 'Not Found'
+            path = os.path.join(os.getcwd(), 'src', 'windows', 'profile', 'avatar.jpg')
+            answer = requests.get(f'{URL}/get_user_picture/profile_picture',  json={'state': f'{state}', 'login': f'{login}'})
+            if answer.status_code == 200:
+                with open(path, mode='wb') as file:
+                    file.write(answer.content)
+                answer = requests.get(f'{URL}/get_user_info', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def new_object(self, object, name, price, description, adress_from, adress_to, time, fee):
         data = self.get_login()
-        if data != []: state, login = data[0], data[1]
+        if data != []: login = data[1]
         else: return 'ты че натворил'
         logging.info(f'new_object: {object} {name} {price} {description} {adress_from} {adress_to}')
-        if object == 'template':
-            answer = requests.post(f'{URL}/new_template', json={'owner': f'{login}', 'name': f'{name}', 'cost': f'{price}', 'description': f'{description}', 'start': f'{adress_from}', 'finish': f'{adress_to}', 'time': f'{time}', 'fee': f'{fee}'})
-        else:
-            print(f'{URL}/new_order/{object}')
-            answer = requests.post(f'{URL}/new_order/{object}', json={'owner': f'{login}', 'name': f'{name}', 'cost': f'{price}', 'description': f'{description}', 'start': f'{adress_from}', 'finish': f'{adress_to}', 'time': f'{time}', 'fee': f'{fee}'})
+        try:
+            if object == 'template':
+                answer = requests.post(f'{URL}/new_template', json={'owner': f'{login}', 'name': f'{name}', 'cost': f'{price}', 'description': f'{description}', 'start': f'{adress_from}', 'finish': f'{adress_to}', 'time': f'{time}', 'fee': f'{fee}'})
+            else:
+                print(f'{URL}/new_order/{object}')
+                answer = requests.post(f'{URL}/new_order/{object}', json={'owner': f'{login}', 'name': f'{name}', 'cost': f'{price}', 'description': f'{description}', 'start': f'{adress_from}', 'finish': f'{adress_to}', 'time': f'{time}', 'fee': f'{fee}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def order_operation(self, operation, type, order_id): # operation = take/complete/delete
@@ -131,12 +168,16 @@ class ServerLogic():
         else: return 'ты че натворил'
         logging.info(f'{operation}_order: {state} {login}')
         type = type.rsplit('_orders', 1)[0]
-        if operation == 'delete':
-            answer = requests.delete(f'{URL}/{operation}_order/{type}/{order_id}', json={'state': f'{state}', 'login': f'{login}'})
-        elif operation == 'take':
-            answer = requests.put(f'{URL}/{operation}_order/{type}/{order_id}', json={'state': f'{state}', 'login': f'{login}'})
-        else:
-            answer = requests.put(f'{URL}/{operation}_order/{order_id}', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            if operation == 'delete':
+                answer = requests.delete(f'{URL}/{operation}_order/{type}/{order_id}', json={'state': f'{state}', 'login': f'{login}'})
+            elif operation == 'take':
+                answer = requests.put(f'{URL}/{operation}_order/{type}/{order_id}', json={'state': f'{state}', 'login': f'{login}'})
+            else:
+                answer = requests.put(f'{URL}/{operation}_order/{order_id}', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_archive_orders(self):
@@ -144,7 +185,11 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_archive: {state} {login}')
-        answer = requests.get(f'{URL}/get_archive_orders', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_archive_orders', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def get_rating(self):
@@ -152,10 +197,18 @@ class ServerLogic():
         if data != []: state, login = data[0], data[1]
         else: return 'ты че натворил'
         logging.info(f'get_rating: {state} {login}')
-        answer = requests.get(f'{URL}/get_user_rating', json={'state': f'{state}', 'login': f'{login}'})
+        try:
+            answer = requests.get(f'{URL}/get_user_rating', json={'state': f'{state}', 'login': f'{login}'})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
     
     def rate_order(self, order_id, num):
         logging.info(f'rate_order: {order_id} {num}')
-        answer = requests.put(f'{URL}/rate_order/{order_id}', data={'rating': num})
+        try:
+            answer = requests.put(f'{URL}/rate_order/{order_id}', data={'rating': num})
+        except requests.exceptions.ConnectionError:
+            logging.info('Server is down')
+            return 'server_error'
         return self.check_status(answer)
